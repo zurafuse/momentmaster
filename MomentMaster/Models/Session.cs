7537a7f81cd38c6943963ae3b09a97c105d61c47
Timothy Horton <zurafuse@gmail.com>
2020-01-12 23:06:05 -0600

    Add files via upload

---
+++ MomentMaster/Models/Session.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MomentMaster.Models
+{
+    public class Session
+    {
+        public Session(string key, string user, DateTime created)
+        {
+            this.Key = key;
+            this.User = user;
+            this.Created = created;
+        }
+        public string User { get; set; }
+        public DateTime Created { get; set; }
+        public string Key { get; set; }
+    }
+}
